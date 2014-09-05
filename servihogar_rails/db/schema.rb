95edd4759f07488e73c35cb3484dbd6c33302d98
andreadelrio <delrio.andre@gmail.com>
2014-09-05 11:44:05 -0500

    changed to psql

--- servihogar_rails/db/schema.rb
+++ servihogar_rails/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140902231431) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: true do |t|
     t.string "name"
   end
@@ -22,8 +25,8 @@ ActiveRecord::Schema.define(version: 20140902231431) do
     t.integer "location_id"
   end
 
-  add_index "localizations", ["location_id"], name: "index_localizations_on_location_id"
-  add_index "localizations", ["provider_id"], name: "index_localizations_on_provider_id"
+  add_index "localizations", ["location_id"], name: "index_localizations_on_location_id", using: :btree
+  add_index "localizations", ["provider_id"], name: "index_localizations_on_provider_id", using: :btree
 
   create_table "locations", force: true do |t|
     t.string "name"
@@ -41,6 +44,6 @@ ActiveRecord::Schema.define(version: 20140902231431) do
     t.string   "password_digest"
   end
 
-  add_index "providers", ["category_id"], name: "index_providers_on_category_id"
+  add_index "providers", ["category_id"], name: "index_providers_on_category_id", using: :btree
 
 end
