9c2488839c5515586be48524e7bbcda55c296357
andreadelrio <delrio.andre@gmail.com>
2014-09-05 11:01:27 -0500

    before adding the new homepage

--- servihogar_rails/features/provider/provider_update_account.feature
+++ servihogar_rails/features/provider/provider_update_account.feature
@@ -1,13 +1,13 @@
-# Feature: provider update account
+Feature: provider update account
 
-#   As a provider
-#   I want to update my account
-#   So that my information is up to date
+  As a provider
+  I want to update my account
+  So that my information is up to date
 
-#   Background:
-#     Given a logged in provider
-#     And I am on my provider edit page
+  Background:
+    Given a logged in provider
+    And I am on my provider edit page
 
-#   Scenario: successfully updated to male
-#     When I update my provider information
-#     Then I should have successfully updated my provider information
+  Scenario: successfully updated category
+    When I update my provider category
+    Then I should have successfully updated my provider information
--- servihogar_rails/features/step_definitions/provider/provider_update_account_steps.rb
+++ servihogar_rails/features/step_definitions/provider/provider_update_account_steps.rb
@@ -1,16 +1,15 @@
-When /^I update my provider information$/ do
-  subcategory = Subcategory.first.name
+When /^I update my provider category$/ do
+  category = Category.first.name
   fill_in('provider[email]', with: @provider.email)
-  fill_in('provider[second_email]', with: 'second_email@example.com')
-  fill_in('provider[first_name]', with: @provider.first_name)
-  fill_in('provider[last_name]', with: @provider.last_name)
-  select(subcategory, from: 'provider[subcategory_id]')
-  select("Lic.", from: 'provider[title]')
+  fill_in('provider[name]', with: @provider.name)
+  fill_in('provider[last_name_1]', with: @provider.last_name_1)
+  fill_in('provider[last_name_2]', with: @provider.last_name_2)
+  select(category, from: 'provider[category_id]')
   click_on("submit")
 end
 
-Then /^I should have successfully updated my provider information$/ do
-  current_path_is dashboard_provider_es_path(@provider)
+When /^I update my provider category$/ do
+  current_path_is edit_provider_path(@provider)
   provider = Provider.find(@provider.id)
   expect(provider.gender).to be_false
   expect(provider.second_email).to eq("second_email@example.com")
