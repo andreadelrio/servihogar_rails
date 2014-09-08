3ef18f97c00118051440a71066b4c03639e6826d
andreadelrio <delrio.andre@gmail.com>
2014-09-08 17:54:58 -0500

    before adding filters

---
+++ servihogar_rails/app/controllers/categories_controller.rb
@@ -0,0 +1,31 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+  before_action :require_current_provider, only: [:edit]
+  before_action :get_ranges, only: [:edit, :new]
+  before_action :require_unlogged_provider, only: [:new]
+
+
+  # GET /providers
+  # GET /providers.json
+  def index
+    @providers = Provider.all
+  end
+
+  # GET /providers/1
+  # GET /providers/1.json
+  def show
+    @providers = @category.providers
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def provider_params
+      params.require(:provider).permit(:name, :last_name_1, :last_name_2, :dni, :email, :category_id, :password, :password_confirmation, locations_attributes: [:id], location_ids: [])
+    end
+
+end
