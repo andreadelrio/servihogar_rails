56d26632402d6f51ff3e809b95aec8398ad0bf26
andreadelrio <delrio.andre@gmail.com>
2014-09-11 11:36:15 -0500

    checkbox for all works as expected, need to add it to edit

--- servihogar_rails/app/assets/javascripts/application.js
+++ servihogar_rails/app/assets/javascripts/application.js
@@ -22,28 +22,56 @@ $(document).ready(function() {
 	$(function () {
 		$('.example').popover();
 	});
-
 });
 
 
-function checkAll(check_all){
-// Pass in a named "Check All" checkbox that appears on the same form where all 
-// checkboxes should be checked.
+function checkAll(){
+	// Pass in a named "Check All" checkbox that appears on the same form where all 
+	// checkboxes should be checked.
+	main_checkbox = document.getElementById("check_all");
 
-// Loop through an array containing ALL inputs on same form as check_all
-var inputs = check_all.form.getElementsByTagName("input");
-for (var i = 0; i < inputs.length; i++) {  
-  // Only work on checkboxes, and NOT on the "Check All" checkbox
-  if (inputs[i].type == "checkbox" && inputs[i].name != check_all.name) { 
-    if(check_all.checked == true){
-      inputs[i].checked = true ;
+	// Loop through an array containing ALL inputs on same form as check_all
+	location_checkboxes = document.getElementsByClassName("location_cbx");
+	for (var i = 0; i < location_checkboxes.length; i++) {  
+	  // Only work on checkboxes, and NOT on the "Check All" checkbox
+    if(main_checkbox.checked == true){
+      location_checkboxes[i].checked = true ;
     }else{
-      inputs[i].checked = false ;
+      location_checkboxes[i].checked = false;
     }
-  }  
-}  
+	};
 }
 
+function uncheckMain(location_checkbox) {
+	var tracker = 0;
+	main_checkbox = document.getElementById("check_all");
+		if(location_checkbox.checked == false) {
+			main_checkbox.checked = false;
+		};
+	location_checkboxes = document.getElementsByClassName("location_cbx");	
+		for (var i = 0; i < location_checkboxes.length; i++) {  
+		  // Only work on checkboxes, and NOT on the "Check All" checkbox
+		  if (location_checkboxes[i].type == "checkbox") { 
+		    if(location_checkboxes[i].checked == true){
+		      tracker ++;
+		    }
+		  }  
+		};
+	  if (tracker == location_checkboxes.length) {
+	    main_checkbox.checked = true;
+	  }
+}
+
+function allLabel() {
+	main_checkbox = document.getElementById("check_all");
+	if (main_checkbox.checked == true) {
+		main_checkbox.checked = false;
+		checkAll();
+	}else{
+		main_checkbox.checked = true;
+		checkAll();
+	}
+}
 
 
 
